03b43bd5ba9b5cfcf7d8a73fc2827f66daa1e885
Alex Fourmy <alexfourmy@gmail.com>
2021-10-28 15:23:34 +0200

    Animated the loader on window loading

--- js/script.js
+++ js/script.js
@@ -4,6 +4,7 @@ const cartItem = document.querySelector('.cart-items-container')
 const menuBtn = document.getElementById('menu-btn')
 const cartBtn = document.getElementById('cart-btn')
 const searchBtn = document.getElementById('search-btn')
+const loaderContainer = document.querySelector('.loader-container')
 
 menuBtn.addEventListener('click', () => {
   navbar.classList.toggle('active')
@@ -28,3 +29,13 @@ window.onscroll = () => {
   cartItem.classList.remove('active')
   searchForm.classList.remove('active')
 }
+
+function loader() {
+  loaderContainer.classList.add('fade-out')
+}
+
+function fadeOut() {
+  setInterval(loader, 3000)
+}
+
+window.onload = fadeOut()
